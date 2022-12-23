63d8d996ba75c5648fb3d0b9d3db4b10ad990c80
Edson Domingos da Silva <edomingoswork@gmail.com>
2022-12-23 10:18:11 -0300

    Contrução do site exemplo em django

--- prjDjango1/urls.py
+++ prjDjango1/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponse
+from recipes.views import home, sobre, contato
+
+def my_view(request):
+    # HTTP RESPONSE
+    return HttpResponse('<h1>Hello Django</h1>')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('recipes.urls')),
+    path('home/', home),
+    path('sobre0/', my_view),
+    path('sobre01/', sobre),
+    path('contato0/', contato),
 ]
