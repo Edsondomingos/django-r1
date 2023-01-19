e347a9151cd9e942797ac2ff77658b6a66ed994e
Edson Domingos da Silva <edomingoswork@gmail.com>
2023-01-19 17:48:43 -0300

    update

--- how_make/views.py
+++ how_make/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'how_make/pages/home.htm', context={})
+    return render(request,'how_make/pages/home.htm', context={'name':'Edson Domingos'})
