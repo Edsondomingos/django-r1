cb94b9cf7ff507f35684efa815e44ec3dd56823f
Edson Domingos da Silva <edomingoswork@gmail.com>
2023-02-01 10:28:15 -0300

    update

--- django-r1/recipes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-# from recipes.views import home, sobre, contato
-from . import views
-
-# HTTP Request
-urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
-]
