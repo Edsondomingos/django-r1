7f47f5edf7b9e6298df14d2b37aa992bfc162acc
Edson Domingos da Silva <edomingoswork@gmail.com>
2023-02-10 08:23:32 -0300

    update temporario

--- recipes/views.py
+++ recipes/views.py
@@ -6,12 +6,13 @@ from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'recipe': make_recipe(),'recipes': [make_recipe() for _ in range(3)],
+        'recipes': [make_recipe() for _ in range(3)],
     })
  
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipes': make_recipe(),
+        'id': id,
     })
 
 def contact(request, id):
